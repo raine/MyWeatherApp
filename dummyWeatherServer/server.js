1a79b4c46870db9e5c7977a6c38b6188ca8784a7
Raine Virta <raine.virta@gmail.com>
2013-11-09 23:38:13 +0200

    add a dummyWeatherServer to serve weather data as json

---
+++ dummyWeatherServer/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app     = express();
+var fs      = require('fs');
+var _	 	= require('underscore');
+ 
+var resTemplate = JSON.parse(fs.readFileSync('weather_res_helsinki.json', 'utf8'));
+
+function naiveClone(obj) {
+	return JSON.parse(JSON.stringify(obj));
+}
+
+function randomFromInterval(from, to) {
+    return Math.floor(Math.random()*(to-from+1)+from);
+}
+
+app.get('/weather', function(req, res) {
+	var resObj = naiveClone(resTemplate);
+	var params = req.query;
+	var query  = params.q; // The location query param
+
+	console.log(params);
+
+	resObj.main.temp = randomFromInterval(253.15, 293.15)
+	resObj.name = query;
+
+	res.json(resObj);
+});
+
+app.listen(3000);
